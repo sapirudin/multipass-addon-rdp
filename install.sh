84786c7dc2e235f8d8706f45969c4f740d9238c0
Andrea Manzi <manzolo@libero.it>
2023-01-29 11:22:43 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 echo "installing rdp..."
 #!/bin/bash
-sudo apt -qqy install ubuntu-desktop
+sudo apt -qqy install --no-install-recommends ubuntu-desktop
 sudo apt -qqy install xfce4 xfce4-goodies xrdp
 echo ""exec startxfce4"" | sudo tee -a /etc/xrdp/xrdp.ini
 sudo adduser xrdp ssl-cert
